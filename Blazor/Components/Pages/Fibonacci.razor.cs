72f790801884119c089b5e40bbdfe15986924e3a
Mona Dmitriy <skorobei91@mail.ru>
2026-09-05 12:12:01 +0300

    Fibonacci DONE

---
+++ Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,28 @@
+﻿namespace Blazor.Components.Pages
+{
+	public partial class Fibonacci
+	{
+		private int n = 10;
+		private long result = 55;
+
+		private void Calculate()
+		{
+			result = GetFibonacci(n);
+		}
+
+		private long GetFibonacci(int n)
+		{
+			if (n <= 0) return 0;
+			if (n == 1) return 1;
+
+			long a = 0, b = 1;
+			for (int i = 2; i <= n; i++)
+			{
+				long temp = a + b;
+				a = b;
+				b = temp;
+			}
+			return b;
+		}
+	}
+}
