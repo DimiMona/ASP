5d0db551005d6e9f72b6c3e16ab5429bc1162f6e
Mona Dmitriy <skorobei91@mail.ru>
2026-09-05 11:46:25 +0300

    Power Done

---
+++ Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Blazor.Components.Pages
+{
+	public partial class Power
+	{
+		double number = 0;
+		double powerValue = 0;
+		double result = 0;
+		void Calculate()
+		{
+			result = Math.Pow(number, powerValue);
+		}
+	}
+}
