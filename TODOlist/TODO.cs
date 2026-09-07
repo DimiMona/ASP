d9faa629b3a974b46122d34b6c0b20f59c0266e6
Mona Dmitriy <skorobei91@mail.ru>
2026-09-07 22:01:38 +0300

    TaskList duplicates Eliminated

---
+++ TODOlist/TODO.cs
@@ -0,0 +1,16 @@
+﻿namespace TODOlist
+{
+	public class TODO
+	{
+		public string Description { get; set; }
+		public bool DONE { get; set; }
+		public DateTime AssignTime { get; set; }
+		public override bool Equals(object? other)
+		{
+			return
+				other is TODO ?
+				this.Description.Equals((other as TODO).Description, StringComparison.OrdinalIgnoreCase) :
+				false;
+		}
+	}
+}
